caf4cd48ee918604bd36ae7d8588d62ef7134515
AntonRykachevskiy <AntonRykachevskiy@users.noreply.github.com>
2016-08-31 01:55:18 +0400

    Update run_comparer.sh

--- usage_example/run_comparer.sh
+++ usage_example/run_comparer.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+
+if [ -z "$1" -a -z "$2" ]; then
+   echo 'Not enough folders provided'
+fi
+
 python .././comparer.py -if ./data/$1 ./data/$2 -bn $1 $2 -rq READ -p -oi $1_$2\_percentiles.jpg -v 3 -of log.out -chp 50 90 95 98 99 99.9
